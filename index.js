1a2ff5aad4880e42c446279a94f4ddd3bab714cf
Heather Mosley <hmosley@student.fullsail.edu>
2022-06-05 17:18:02 -0500

    added product models, controllers, and routes

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: false}))
+const productRouter = require('./routes/Products')
 
-// GET /products/all HTTP/1.1
-app.get('/products/all', (req, res) => {
-    if(req.query !== "{}"){
-        res.send(`Products: ${req.headers.page}, ${req.headers.sort}, ${req.headers.order}`)
-    }else{
-        res.send(req.query)
-    }
-    //res.send(`Product: ${req.query.id}`)
-})
+app.use("/products", productRouter)
 
-// GET /products/8719 HTTP/1.1
-app.get('/products/:id', (req, res) => {
-    res.send(
-        `Product:  ${req.params.id}`)
-})
-
-// GET /products/8719-small-red HTTP/1.1
-app.get('/products/:id-:size-:color', (req, res) => {
-    res.send(
-        `Product: ${req.params.id}, ${req.params.size}, ${req.params.color}`)
-})
 
 app.listen(3000)
