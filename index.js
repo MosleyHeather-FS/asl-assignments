34a3ba383056f2b4ffe7d509d0ba8816c7eae6f0
Heather Mosley <hmosley@student.fullsail.edu>
2022-06-05 16:28:31 -0500

    updated index.js

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 // GET /products/all HTTP/1.1
-app.get('/products/all', (req, res) => {
-    if(req.query !== "{}"){
-        res.send(`Products: ${req.headers.page}, ${req.headers.sort}, ${req.headers.order}`)
-    }else{
-        res.send(req.query)
-    }
-    //res.send(`Product: ${req.query.id}`)
-})
+app.get("/products/all", (req, res) => {
+  if (req.query !== "{}") {
+    res.send(
+      `Products: ${req.headers.page}, ${req.headers.sort}, ${req.headers.order}`
+    );
+  } else {
+    res.send(req.query);
+  }
+});
 
 // GET /products/8719 HTTP/1.1
-app.get('/products/:id', (req, res) => {
-    res.send(
-        `Product:  ${req.params.id}`)
-})
+app.get("/products/:id", (req, res) => {
+  res.send(`Product:  ${req.params.id}`);
+});
 
 // GET /products/8719-small-red HTTP/1.1
-app.get('/products/:id-:size-:color', (req, res) => {
-    res.send(
-        `Product: ${req.params.id}, ${req.params.size}, ${req.params.color}`)
-})
+app.get("/products/:id-:size-:color", (req, res) => {
+  res.send(
+    `Product: ${req.params.id}, ${req.params.size}, ${req.params.color}`
+  );
+});
 
-app.listen(3000)
+app.get("/^/pages/([0-9].*)/", (req, res) => {
+  res.send(`Product: ${req.params.pageId}`);
+});
+
+app.listen(3000);
