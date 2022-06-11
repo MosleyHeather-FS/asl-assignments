361b207608e2176a3f5079090f6335a5519b24ae
Heather Mosley <hmosley@student.fullsail.edu>
2022-06-11 18:07:12 -0500

    connected language template

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: false}))
-const productRouter = require('./routes/Products')
+app.set('views', __dirname + '/templates/views')
+app.set('view engine', 'mustache')
 
 app.use("/products", productRouter)
-
 
 app.listen(3000)
