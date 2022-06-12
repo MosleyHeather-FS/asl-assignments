c0e85c6a2ea7395757dc90ae8b84e9b3bf1da45e
Heather Mosley <hmosley@student.fullsail.edu>
2022-06-12 15:33:42 -0500

    switched to twig and added views and partials

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: false}))
-app.set('views', __dirname + '/templates/views')
-app.set('view engine', 'mustache')
+const productRouter = require('./routes/Products')
+
+app.set('views', __dirname + '/templates')
+app.set('view engine', 'twig')
+
+app.get('/', (req, res) => {
+    res.render("home.twig", {name: 'Jim', 'users': [
+        {name: 'Justin', email: 'random1@email.com'},
+        {name: 'Heather', email: 'random2@email.com'},
+        {name: 'Emilia', email: 'random3@email.com'}
+    ]})
+})
 
 app.use("/products", productRouter)
 
---
+++ models/Products.js
@@ -0,0 +1,31 @@
+let products = [{id: "1", "slug": "nike-shoe", "name": "Nike Shoe"}]
+
+//index
+const all = () => {
+    return products
+}
+
+//show
+const find = (id) => {
+    return products.find(p => p.id === id)
+}
+
+const create = (product) => {
+    const id = Number(products[products.length - 1].id) + 1 + ""
+    products.push({id, ...product})
+    return products[products.length - 1]
+}
+
+const update = (id, product) => {
+    products = products.map((p) => {
+        return (p.id === id) ? product : p
+    })
+    return products
+}
+
+const remove = (id) => {
+    products = products.filter(p => p.id !== id)
+    return products
+}
+
+module.exports = {all, find, update, remove, create}
