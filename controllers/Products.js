0e7a61380c141519c49791ae25fbe1894b14c04f
Heather Mosley <hmosley@student.fullsail.edu>
2022-06-12 17:48:10 -0500

    added price

--- controllers/Products.js
+++ controllers/Products.js
@@ -4,7 +4,6 @@ const Products = require('../models/Products')
 const index = (req, res) => {
   const products = Products.all()
   res.render('views/products/index', {products})
-  //res.json(products)
 };
 
 //GET
@@ -15,35 +14,30 @@ const form = (req, res) => {
   } else {
     res.render('views/products/create')
   }
-  //res.send("Product.form");
 };
 
 //GET
 const show = (req, res) => {
   const product = Products.find(req.params.id)
   res.render('views/products/show', {product})
-  //res.json(product)
 };
 
 //POST
 const create = (req, res) => {
   const product = Products.create(req.body)
   res.redirect('/products/' + product.id)
-  //res.json(product)
 };
 
 //POST
 const update = (req, res) => {
   const product = Products.update(req.params.id, req.body)
   res.redirect('/products/' + req.params.id)
-  //res.json(product)
 };
 
 //DELETE and/or POST
 const remove = (req, res) => {
     const products = Products.remove(req.params.id)
-    res.redirect('/products')
-    //res.json(products)
+    res.json(products)
 }
 
 module.exports = { index, form, show, create, update, remove };
