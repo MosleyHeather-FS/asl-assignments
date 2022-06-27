358a891bd7e8971199353f4daac9390c625b7cf3
Heather Mosley <hmosley@student.fullsail.edu>
2022-06-26 20:46:38 -0500

    Added styling using Bootstrap

--- seeders/20220626180844-AddImages.js
+++ seeders/20220626180844-AddImages.js
@@ -3,10 +3,10 @@ const [ createdAt, updatedAt ] = [ new Date(), new Date() ]
 module.exports = {
   async up (queryInterface, Sequelize) {
     return await queryInterface.bulkInsert('Images', [
-      { id: 1, variantId: 1, extension: '1.png', createdAt, updatedAt },
-      { id: 2, variantId: 2, extension: '2.png', createdAt, updatedAt },
-      { id: 3, variantId: 3, extension: '3.png', createdAt, updatedAt },
-      { id: 4, variantId: 4, extension: '4.png', createdAt, updatedAt }
+      { id: 1, variantId: 1, extension: '.png', createdAt, updatedAt },
+      { id: 2, variantId: 2, extension: '.png', createdAt, updatedAt },
+      { id: 3, variantId: 3, extension: '.png', createdAt, updatedAt },
+      { id: 4, variantId: 4, extension: '.png', createdAt, updatedAt }
     ])
   },
   async down (queryInterface, Sequelize) {
